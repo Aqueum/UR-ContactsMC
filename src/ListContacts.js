ed45a26d35c57a07c8a6dab28f622fc94a27415e
Aqueum <martin@aqueum.com>
2017-07-31 09:05:17 +0100

    Undo change to 'show all contacts'

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -53,7 +53,7 @@ class ListContacts extends Component {
             <span>
               Now showing {showingContacts.length} of {contacts.length} contacts
             </span>
-            <button onClick={this.clearQuery}>show all contacts</button>
+            <button onClick={this.clearQuery}>show all</button>
           </div>}
 
         <ol className="contact-list">
